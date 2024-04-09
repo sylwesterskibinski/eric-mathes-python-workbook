b8302adaf59ce9304d64928820e87c3a5e471d4b
Sylwester Skibinski <skibinskisylwester@gmail.com>
2024-04-10 01:20:48 +0200

    added tuples

---
+++ lists/tuples.py
@@ -0,0 +1,10 @@
+menu = ("Spaghetti","Soup","Tea","Caffe","Milk")
+
+# Display all elements of tuple
+for i in menu:
+    print(i)
+
+# Overwrite tuple
+menu = ("Spaghetti","Hummus","Tea","Caffe","Capuccino")
+for i in menu:
+    print(i)
